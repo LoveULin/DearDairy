72e053c76a0e31968ceaa07dbc0daca339ddb898
LoveULin <188002324@qq.com>
2015-09-11 02:08:07 +0800

    cache the key local

--- verifyIOSGameCenterLogin.php
+++ verifyIOSGameCenterLogin.php
@@ -1,5 +1,4 @@
 <?php
-
 function int64ToBytes($val) {
     $byt = array();
     $byt[0] = ($val & 0xff);
@@ -21,8 +20,8 @@ function base64ToBytes($string) {
 }
 function concatPayload($playerId, $bundleId, $timestamp, $salt) {
     $bytes = array_merge(
-        unpack('C*', $playerId), 
-        unpack('C*', $bundleId), 
+        unpack('C*', utf8_encode($playerId)), 
+        unpack('C*', utf8_encode($bundleId)), 
         int64ToBytes(floatval($timestamp)), 
         base64ToBytes($salt)
     );
@@ -35,29 +34,53 @@ function concatPayload($playerId, $bundleId, $timestamp, $salt) {
 }
 function init() {
     $result = 0;
+    $isRefresh = false;
     do {
         if (!isset($_POST['publicKeyURL'])) {
             break;
         }
-        $strm = stream_context_create(array('http' => array('timeout' => 7), 'https' => array('timeout' => 7)));
-        $cnt = 0;
-        while ($cnt < 3) {
-            $pKey = file_get_contents($_POST['publicKeyURL'], false, $strm);
-            if ($pKey) {
+        $pKey = '';
+        $fileFlag = file_exists('./' . base64_encode($_POST['publicKeyURL']));
+        if (!$fileFlag) {
+            $strm = stream_context_create(array('http' => array('timeout' => 7), 'https' => array('timeout' => 7)));
+            $cnt = 0;
+            while ($cnt < 3) {
+                $pKey = file_get_contents($_POST['publicKeyURL'], false, $strm);
+                if ($pKey) {
+                    break;
+                }
+                ++$cnt;
+            }
+            if (!$pKey) {
                 break;
             }
-            ++$cnt;
+            $isRefresh = true;
         }
-        if (!$pKey) {
-            break;
+        else {
+            $fileKey = fopen('./iosGCKey', 'rb');
+            $pKey = fread($fileKey, filesize('./iosGCKey'));
+            fclose($fileKey);
         }
         $public_key = "-----BEGIN CERTIFICATE-----\n" . chunk_split(base64_encode($pKey), 64, "\n") . "-----END CERTIFICATE-----";
         $public_key_handle = openssl_get_publickey($public_key);
+        if (!$public_key_handle) {
+            break;
+        }
         $buffer = concatPayload($_POST['playerID'], $_POST['bundleID'], $_POST['timestamp'], $_POST['salt']);
         $result = openssl_verify($buffer, base64_decode($_POST['signature']), $public_key_handle, OPENSSL_ALGO_SHA256);
+        openssl_free_key($public_key_handle);
     } while(false);
     if (1 == $result) {
         $Result['ret'] = 0;
+        if ($isRefresh) {
+            $theKey = fopen('./iosGCKey', 'wb');
+            if ($theKey) {
+                if (strlen($pKey) == fwrite($theKey, $pKey)) {
+                    fclose($theKey);
+                    touch('./' . base64_encode($_POST['publicKeyURL']));
+                }
+            }
+        }
     }
     else {
         $Result['ret'] = 1;
